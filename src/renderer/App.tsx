c12169183d92a04a50d03fff6de38cfb6e847ce8
Tom-Rune Bornholdt <tr.bornholdt@hotmail.de>
2021-04-29 01:05:26 +0200

    refactor: 💡 cleanup

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -3,14 +3,13 @@ import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 
 function App() {
-  // Create the count state.
   const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
+
   useEffect(() => {
     const timer = setTimeout(() => setCount(count + 1), 1000);
     return () => clearTimeout(timer);
   }, [count, setCount]);
-  // Return the App component.
+
   return (
     <div className="App">
       <header className="App-header">
